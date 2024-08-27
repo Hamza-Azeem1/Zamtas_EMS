e90d67abce4e4b643c74f34b0fdc5bb539def669
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-08-27 14:35:01 +0500

    updated

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import SignIn from '../pages/SIgnIn';
+import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Home from '../pages/Home';
 import UserHome from '../pages/UserHome';
