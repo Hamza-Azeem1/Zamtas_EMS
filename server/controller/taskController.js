3d9e7446890b14747d26096d2900a40bf9b0a53c
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-09-18 15:53:10 +0500

    updated

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -26,7 +26,7 @@ async function addTaskController(req, res) {
             startDate,
             endDate,
             endTime: '',
-            endTime, // Ensure this is passed in the request
+            endTime,
             assignedTo,
             teamLead
         });
@@ -291,30 +291,24 @@ async function updateTaskController(req, res) {
 
 async function checkAndUpdateDelayedTasks() {
     try {
-        const currentDateTime = moment(); // Get the current date and time
+        const currentDateTime = moment();
 
-        // Find tasks that are not done and should be checked for delay
         const tasks = await Task.find({ status: { $in: ['Assigned', 'In Progress'] } });
 
         //   console.log(`Checking ${tasks.length} tasks for delay...`);
 
         for (const task of tasks) {
-            // Combine both the endDate and endTime into a single datetime for comparison
             const taskDeadline = moment(`${task.endDate.toISOString().split('T')[0]} ${task.endTime}`, 'YYYY-MM-DD HH:mm');
             //        console.log(`Task ${task._id}: Deadline is ${taskDeadline.toISOString()}, Current time is ${currentDateTime.toISOString()}`);
 
-            // If the current time is after the deadline, update the task status to 'Delayed'
             if (currentDateTime.isAfter(taskDeadline)) {
-                // Update the task status
                 task.status = 'Delayed';
-                await task.save(); // Save the updated task status
-                //              console.log(`Task ${task._id} marked as delayed.`);
+                await task.save();
+                //console.log(`Task ${task._id} marked as delayed.`);
 
-                // Create notifications for the user assigned to the task
                 for (const userId of task.assignedTo) {
                     const assignedUser = await User.findById(userId);
                     if (assignedUser) {
-                        // Create a notification for the delayed task
                         const notification = new Notification({
                             userId: assignedUser._id,
                             message: `Task "${task.title}" has been marked as delayed.`,
@@ -346,7 +340,7 @@ async function checkAndUpdateDelayedTasks() {
 
 
 cron.schedule('*/5 * * * *', () => {
-    //    console.log('Running task delay check...');
+    //console.log('Running task delay check...');
     checkAndUpdateDelayedTasks();
 });
 
